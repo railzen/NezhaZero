87f8145d662bce8a86ebdb4411a9c732f700f915
RailZen <railzen@126.com>
2025-12-19 01:49:29 +0800

    [CHG]修改登陆失败错误提示,解决设置完成时候成功提示过快消失问题

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+goreleaser build --single-target --clean --skip=validate 
+if [ $? -ne 0 ]; then
+    echo "Command failed, exiting script"
+    exit 0
+fi
+cp -f ./dist/linux_amd64_linux_amd64_v1/dashboard-linux-amd64 /opt/nezha/dashboard/app 
+sudo systemctl restart nezha-dashboard
