abd3380e2b5efb9fffeb0422531944b05a398ba2
RailZen <railzen@126.com>
2026-02-09 02:21:11 +0800

    [CHG]去除多余的判断逻辑

--- script/naza.sh
+++ script/naza.sh
@@ -248,7 +248,7 @@ before_show_menu() {
 }
 
 install_base() {
-    (command -v curl >/dev/null 2>&1 && command -v wget >/dev/null 2>&1 && command -v unzip >/dev/null 2>&1 && command -v getenforce >/dev/null 2>&1) ||
+    (command -v curl >/dev/null 2>&1 && command -v wget >/dev/null 2>&1 && command -v unzip >/dev/null 2>&1) ||
         (install_soft curl wget unzip)
 }
 
