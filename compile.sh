3fd31e81552d7c0f5ba536b8a357074c7cb0f295
RailZen <railzen@126.com>
2026-02-13 02:02:36 +0800

    [CHG]服务界面增加鼠标当前服务器颜色标识

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ if [ $? -ne 0 ]; then
     echo "Command failed, exiting script"
     exit 0
 fi
-cp -f ./dist/linux_amd64_linux_amd64_v1/dashboard-linux-amd64 /opt/nezha/dashboard/app 
+sudo cp -f ./dist/linux_amd64_linux_amd64_v1/dashboard-linux-amd64 /opt/nezha/dashboard/app 
 sudo systemctl restart nezha-dashboard
