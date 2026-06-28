2fb03977aad2357eda96d9c2e042cd33be196085
RailZen <railzen@126.com>
2026-06-28 21:17:50 +0800

    Remove wwads placements from docs theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,11 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import HorizontalAd from './components/HorizontalAd.vue'
-import VerticalAd from './components/VerticalAd.vue'
 import DocAsideannouncement from './components/DocAsideannouncement.vue'
 
 export default {
     ...DefaultTheme,
     Layout() {
         return h(DefaultTheme.Layout, null, {
-            'doc-top': () => h(HorizontalAd),
-            'aside-top': () => h(VerticalAd),
             'aside-ads-before': () => h(DocAsideannouncement)
         })
     }
