29b7a274d9c45937b72262801b70661adff38461
RailZen <railzen@126.com>
2026-07-21 00:51:48 +0800

    [CHG]修改Docker部署也能优雅关闭

--- script/extras/entrypoint.sh
+++ script/extras/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 printf "nameserver 127.0.0.11\nnameserver 8.8.4.4\nnameserver 223.5.5.5\n" > /etc/resolv.conf
-/dashboard/app
+# 用 exec 让 app 替换 shell 成为 PID 1，才能收到 docker stop 的 SIGTERM 触发优雅退出
+exec /dashboard/app
