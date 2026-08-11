e8a62275e9682f7dc0828d5af388ae538d42cf13
RailZen <railzen@126.com>
2026-08-12 00:18:02 +0800

    [CHG]增加固化disable-send-query和disable-nat

--- agent/cmd/agent/service.go
+++ agent/cmd/agent/service.go
@@ -82,6 +82,8 @@ func serviceActions(cmd *cobra.Command, args []string) {
 		{agentCliParam.SkipConnectionCount, "--skip-conn", ""},
 		{agentCliParam.SkipProcsCount, "--skip-procs", ""},
 		{agentCliParam.DisableCommandExecute, "--disable-command-execute", ""},
+		{agentCliParam.DisableNat, "--disable-nat", ""},
+		{agentCliParam.DisableSendQuery, "--disable-send-query", ""},
 		{agentCliParam.DisableAutoUpdate, "--disable-auto-update", ""},
 		{agentCliParam.DisableForceUpdate, "--disable-force-update", ""},
 		{agentCliParam.UseIPv6CountryCode, "--use-ipv6-countrycode", ""},
